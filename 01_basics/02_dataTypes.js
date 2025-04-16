47489fdb871cc8ea22448d08c6d5cb6d4496392a
bhavamsingh <108822278+bhavamsingh@users.noreply.github.com>
2025-04-16 17:28:38 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not borowser
+
+console.log(3
+    +
+    3) // code redability should be high
+
+    console.log("hitesh")
+
+    let name = "hitesh"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolian => true/fales
+    // null => standalone value
+    // undifined =>
+    // symbol => unique
+
+    // object
+
+    console.log(typeof undefined) // undefined
+    console.log(typeof null)   // object
+    
