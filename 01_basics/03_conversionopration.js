b9420e76ffb43991355a62e66827c13f943dbcb1
bhavamsingh <108822278+bhavamsingh@users.noreply.github.com>
2025-04-16 22:13:51 +0000

    opration  in conversion

--- 01_basics/03_conversionopration.js
+++ 01_basics/03_conversionopration.js
@@ -28,5 +28,34 @@ let boolianIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//*********************** Oprations ***************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Hitesh"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true)
