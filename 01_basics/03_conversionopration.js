496862ddfe0f3214092b41711be6770f99b67f28
bhavamsingh <108822278+bhavamsingh@users.noreply.github.com>
2025-04-16 19:50:09 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionopration.js
@@ -0,0 +1,32 @@
+
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+let score = true
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33;
+//  "33abc" => NaN;
+//  true => 1; fales => 0;
+// null =>0;
+// undefined => NaN;
+
+// let isLoggedIn =1
+let isLoggedIn = "Hitesh"
+let boolianIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolianIsLoggedIn)
+
+//1 => true; 0=> false;
+// "" => false ;
+// "hitesh" => true;
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
