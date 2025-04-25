305b1666f51fce3d9dea09ada2538b1f72ee0609
bhavamsingh <108822278+bhavamsingh@users.noreply.github.com>
2025-04-25 09:58:19 +0000

    comparision of datatype

--- 01_basics/03_conversionopration.js
+++ 01_basics/03_conversionopration.js
@@ -58,4 +58,19 @@ let str3 = str1+str2
 
 // console.log((3 + 4) * 5 % 3);
 
-console.log(true)
+// console.log(true)
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 
+
+num1= num2 = num3 = 2+2;
+
+let gamecounter = 100 ;
+
+// gamecounter++
+++gamecounter
+console.log(gamecounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
