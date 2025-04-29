6566a8a8f94dfdc2eaed1dd6c6d9c931dee00ee3
bhavamsingh <108822278+bhavamsingh@users.noreply.github.com>
2025-04-29 11:59:03 +0000

    string concept

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+
+const name = "hitesh"
+const repoCount= 50
+
+// console.log(name + repoCount + "value")
+
+console.log(`Hello my name is ${name} and repo Count ${repoCount}`)
+const gameName= new String("Hitesh-hc")
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)
+
+console.log(newString)
