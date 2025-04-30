95648fb2896bb5619ed3559d178faf50eb3703b0
bhavamsingh <108822278+bhavamsingh@users.noreply.github.com>
2025-04-30 11:06:09 +0000

    string changes first

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,7 +5,7 @@ const repoCount= 50
 // console.log(name + repoCount + "value")
 
 console.log(`Hello my name is ${name} and repo Count ${repoCount}`)
-const gameName= new String("Hitesh-hc")
+const gameName= new String("Hitesh-hc-hc")
 
 // console.log(gameName[0])
 // console.log(gameName.__proto__)
@@ -17,3 +17,18 @@ console.log(gameName.indexOf('t'))
 const newString = gameName.substring(0,4)
 
 console.log(newString)
+const anotherString = gameName.slice(-8,4)
+
+console.log(anotherString)
+
+const newStringOne = "  Hitesh  "
+
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https/hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','_'))
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split('-'))
